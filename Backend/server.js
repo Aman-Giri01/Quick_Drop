e856dc11d88a80fa622365f63a17f6591fe70338
Aman-Giri01 <amankumargiri2022@gmail.com>
2026-01-24 21:23:56 +0530

    Structure,Database Added

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+import { connectDB } from "./src/config/db.js";
+
+await connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, (err) => {
+      if (err) {
+        console.log(err);
+        console.log(`Error while starting the server`);
+        process.exit(1);
+      } else console.log(`Server running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error while connecting to database`);
+    console.log(err);
+    process.exit(1);
+  });
